ef15040b24e53b57e4b54aed1ccb449f5338c24b
soma11soma11 <h2so4cos0@gmail.com>
2018-05-03 13:45:36 +0100

    added map

---
+++ main.js
@@ -0,0 +1,10 @@
+var map = L.map('map').setView([36.3219088　, 139.0032936], 14);
+ 
+
+L.tileLayer(
+	'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+	{
+		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
+		maxZoom: 18
+	}
+).addTo(map);
