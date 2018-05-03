6a6ae8c4b9258de8eb46cf0ce7e3d95f52d124a5
soma11soma11 <h2so4cos0@gmail.com>
2018-05-03 14:19:44 +0100

    added map

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-var map = L.map('map').setView([36.3219088　, 139.0032936], 14);
- 
+var map = L.map('map').setView([51.505, -0.09], 13);
 
-L.tileLayer(
-	'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-	{
-		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
-		maxZoom: 18
-	}
-).addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
