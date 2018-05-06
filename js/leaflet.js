b68acac8738c3ffa8666bbf7fac42e1cb0843210
Ilja <iljapanic@gmail.com>
2018-05-06 11:35:10 +0100

    minor code refactoring

--- js/leaflet.js
+++ js/leaflet.js
@@ -1,17 +1,31 @@
-var map = L.map('mapContainer').setView([51.5, -0.09], 13);
+// set initial view
+var london_center = [51.5, -0.09];
+var initial_zoom = 13
+
+// create a leaflet instance
+var map = L.map('mapContainer').setView(london_center, initial_zoom);
 
 
-L.tileLayer('https://api.mapbox.com/styles/v1/11soma/cjgry9jr3000z2roggiqpzehx/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiMTFzb21hIiwiYSI6ImNqZDBuMjV6dDF2bWcyeG8xNDByZXpjbjgifQ.5B5BJIcEtqFELPs36GUxcA', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
+// tile settings
+var mapbox_tiles = 'https://api.mapbox.com/styles/v1/11soma/cjgry9jr3000z2roggiqpzehx/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiMTFzb21hIiwiYSI6ImNqZDBuMjV6dDF2bWcyeG8xNDByZXpjbjgifQ.5B5BJIcEtqFELPs36GUxcA';
+
+var map_attribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>';
+
+var max_zoom = 18;
+
+// add custom tiles to the map
+L.tileLayer(mapbox_tiles, {
+    attribution: map_attribution,
+    maxZoom: max_zoom,
     id: 'mapbox.mapbox-streets-v7',
 }).addTo(map);
 
+
+// create a custom pulsating marker
 var pulsingIcon = L.icon.pulse({iconSize:[8,8],color:'#C70039'});
 
 // var marker = L.marker([51.5, -0.09],{icon: pulsingIcon}).addTo(map);
 // marker.bindPopup("a");
-
 
 
 var marker = L.marker([51.5, -0.087],{icon: pulsingIcon}).addTo(map);
