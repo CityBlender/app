804216faad5a74651ef47948cca2f7623cfaaf51
soma11soma11 <h2so4cos0@gmail.com>
2018-05-23 08:53:24 +0100

    about iframe

--- about/js/about.js
+++ about/js/about.js
@@ -1,19 +1,40 @@
 $(document).ready(function () {
-  var ifrm = document.createElement("iframe");
-  ifrm.setAttribute("src", "//plot.ly/~cohenjota/79.embed");
+
+  function returnIframe(iframe_url) {
+    var iframe = document.createElement("iframe");
+    iframe.setAttribute('src', iframe_url);
+    iframe.setAttribute('frameBorder', 0);
+    return iframe;
+  }
+
+  $('.iframe-inject .nav-link').click(function() {
+
+    var target_href = $(this).attr('href');
+    var iframe_url = $(this).attr('data-iframe');
+    var iframe_element = returnIframe(iframe_url)
+    var target_element = $(target_href);
+
+    // delete existing
+    target_element.find('iframe').remove();
+
+    // then add new one
+    target_element.append(iframe_element);
+
+  });
+
   // What type of music is out there?
+  var ifrm_genre = document.createElement("iframe");
+  ifrm_genre.setAttribute("src", "//plot.ly/~cohenjota/88.embed");
+  ifrm_genre.setAttribute('frameBorder', 0);
   $( "#Genre-tab" ).click(function() {
-    document.getElementById('Genre').appendChild(ifrm);
+    document.getElementById('Genre').appendChild(ifrm_genre);
   });
   $( "#words-tab" ).click(function() {
-    ifrm.remove()
+    ifrm_genre.remove()
   });
-  ifrm.remove()
 
 
 
 
-
-  // get
 });
 
