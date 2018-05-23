200a94b6fe04e6766fd8743964b8bf444345fc5e
soma11soma11 <h2so4cos0@gmail.com>
2018-05-23 05:57:55 +0100

    fixed remove

--- about/js/about.js
+++ about/js/about.js
@@ -1,14 +1,15 @@
 $(document).ready(function () {
-
-  // listen on click of tab
+  var ifrm = document.createElement("iframe");
+  ifrm.setAttribute("src", "//plot.ly/~cohenjota/79.embed");
+  // What type of music is out there?
   $( "#Genre-tab" ).click(function() {
-    var ifrm = document.createElement("iframe");
-    ifrm.setAttribute("src", "//plot.ly/~cohenjota/79.embed");
-    ifrm.frameBorder = 0;
-    ifrm.style.width = "1400";
-    ifrm.style.height = "100";
     document.getElementById('Genre').appendChild(ifrm);
   });
+  $( "#words-tab" ).click(function() {
+    ifrm.remove()
+  });
+  ifrm.remove()
+
 
 
 
